418fa223c500377c770cbce8b722536855b67630
rmingon <ronanmingon69550@gmail.com>
2024-08-14 22:32:34 +0200

    add mpu6050

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -8,6 +8,10 @@ SPIClass LoRaSPI;
 #include <Adafruit_BMP280.h>
 Adafruit_BMP280 bmp;
 
+#include <MPU6050_light.h>
+MPU6050 mpu(Wire);
+long timer = 0;
+
 #include <ESP32Servo.h>
 Servo myservo;
 
@@ -39,7 +43,7 @@ void setup(){
     myservo.write(0);
     delay(1000);
     myservo.write(180);
-    
+
     LoRaSPI.begin(14, 12, 13);
     LoRa.setSPI(LoRaSPI);
     LoRa.setPins(15, 16, 17);
@@ -57,11 +61,44 @@ void setup(){
                 Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                 Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                 Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+    byte status = mpu.begin();
+    Serial.print(F("MPU6050 status: "));
+    Serial.println(status);
+    while(status!=0){ } // stop everything if could not connect to MPU6050
+    
+    Serial.println(F("Calculating offsets, do not move MPU6050"));
+    delay(1000);
+    mpu.calcOffsets(true,true); // gyro and accelero
+
     LoRa.onReceive(onReceive);
     LoRa.receive();
 } 
  
 void loop(){
+
+    mpu.update();
+
+  if(millis() - timer > 1000){ // print data every second
+    Serial.print(F("TEMPERATURE: "));Serial.println(mpu.getTemp());
+    Serial.print(F("ACCELERO  X: "));Serial.print(mpu.getAccX());
+    Serial.print("\tY: ");Serial.print(mpu.getAccY());
+    Serial.print("\tZ: ");Serial.println(mpu.getAccZ());
+  
+    Serial.print(F("GYRO      X: "));Serial.print(mpu.getGyroX());
+    Serial.print("\tY: ");Serial.print(mpu.getGyroY());
+    Serial.print("\tZ: ");Serial.println(mpu.getGyroZ());
+  
+    Serial.print(F("ACC ANGLE X: "));Serial.print(mpu.getAccAngleX());
+    Serial.print("\tY: ");Serial.println(mpu.getAccAngleY());
+    
+    Serial.print(F("ANGLE     X: "));Serial.print(mpu.getAngleX());
+    Serial.print("\tY: ");Serial.print(mpu.getAngleY());
+    Serial.print("\tZ: ");Serial.println(mpu.getAngleZ());
+    Serial.println(F("=====================================================\n"));
+    timer = millis();
+  }
+
   if (bmp.takeForcedMeasurement()) {
     // can now print out the new measurements
     Serial.print(F("Temperature = "));
