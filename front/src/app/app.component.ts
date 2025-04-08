67a94d1da3e41fe7a49e6501b5721f5af2483e95
rmingon <ronanmingon69550@gmail.com>
2025-04-08 21:22:16 +0200

    cindt je t"aime z t lz qioq d

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WsService } from './ws.service';
+import { Msg, WsService } from './ws.service';
 import { FormsModule } from '@angular/forms';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { Icon, icon, latLng, Layer, marker, tileLayer } from 'leaflet';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, LeafletModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,13 +17,67 @@ export class AppComponent implements OnInit {
 
   url: string = '';
 
-  constructor(private wsService: WsService) {}
-    ngOnInit() {
+  layers: Layer[] = [];
+
+  cmds: Msg[] = []
+  chart: any = [];
+
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(45.750000, 4.850000)
+  };
+
+  constructor(private wsService: WsService) { }
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'rssi',
+            data: [],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
   }
 
   addServerUrl() {
-    console.log(this.url);
     this.wsService.setServer(this.url);
+
+    this.wsService.latLng?.subscribe((msg) => {
+      console.log(msg);
+      this.layers.push(marker([msg.lat, msg.lon], {
+        icon: icon({
+          ...Icon.Default.prototype.options,
+          iconUrl: 'assets/marker-icon.png',
+          iconRetinaUrl: 'assets/marker-icon-2x.png',
+          shadowUrl: 'assets/marker-shadow.png'
+        }),
+      }));
+      this.options.center = latLng(msg.lat, msg.lon);
+      this.options.zoom = 10;
+    });
+
+    this.wsService.subject?.subscribe((cmd) => {
+      this.chart.data.labels.push(cmd.kmh);
+      this.chart.data.datasets[0].data.push(cmd.kmh);
+    });
   }
-  
+
+
+
 }
--- front/src/app/ws.service.ts
+++ front/src/app/ws.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+export interface Msg {
+  lat: number;
+  lon: number;
+  kmh: number;
+  x: number;
+  y: number;
+  rssi: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class WsService {
-    subject: WebSocketSubject<any> | null = null;
+  subject: WebSocketSubject<Msg> | null = null;
+  latLng: Observable<Pick<Msg, 'lat' | 'lon'>> | null = null;
+  rssi: Observable<Pick<Msg, 'rssi'>> | null = null;
 
   constructor() { }
 
   setServer(server: string) {
     this.subject = webSocket(server);
     this.subject.subscribe({
-        next: msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
-        error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
-        complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
-       });
+      next: msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    });
+
+    this.latLng = this.subject.pipe(map((msg) => ({ lat: msg.lat, lon: msg.lon })));
+    this.rssi = this.subject.pipe(map((msg) => ({ rssi: msg.rssi })));
+
   }
 }
